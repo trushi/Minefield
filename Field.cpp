52db6a3558136f6751c04cd02cdc17ec4b9e6683
Rushi Thakkar <trushi@users.noreply.github.com>
2016-03-07 10:59:39 -0800

    Updated isSafe function

--- Field.cpp
+++ Field.cpp
@@ -51,7 +51,21 @@ FieldType Field::get(int x, int y)
  bool Field::isSafe(int x, int y)
  {
  	//TODO: Complete this function, isSafe(int,int)
- 	return true;
+	if(x < 0 || x >= FIELD_DIMENSION || y < 0 || y >= FIELD_DIMENSION)
+ 	{
+  		throw "Out of bounds";
+  	}
+  	else{
+    		if(_map[x][y] == MINE_HIDDEN || _map[x][y] == MINE_SHOWN){
+  	    	return false;
+     	}
+     	else{
+       		return true;
+    }
+   }
+  	//return true;
+   }	
+	return true;
  }
 
 /**
